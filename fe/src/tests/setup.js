c0270400fd6ac7310757275cecc6611411e4f00f
lchsk <mjlechowski@gmail.com>
2020-04-21 09:01:53 +0100

    Prettier on some tests

--- fe/src/tests/setup.js
+++ fe/src/tests/setup.js
@@ -2,16 +2,16 @@ const o = require("ospec");
 const jsdom = require("jsdom");
 
 var dom = new jsdom.JSDOM("", {
-    // So we can get `requestAnimationFrame`
-    pretendToBeVisual: true,
-})
+  // So we can get `requestAnimationFrame`
+  pretendToBeVisual: true
+});
 
-global.window = dom.window
-global.document = dom.window.document
-global.requestAnimationFrame = dom.window.requestAnimationFrame
+global.window = dom.window;
+global.document = dom.window.document;
+global.requestAnimationFrame = dom.window.requestAnimationFrame;
 
-require("mithril")
+require("mithril");
 
 o.after(function() {
-    dom.window.close()
-})
+  dom.window.close();
+});
--- fe/src/tests/signup.js
+++ fe/src/tests/signup.js
@@ -1,15 +1,15 @@
-var mq = require("mithril-query")
-var o = require("ospec")
+var mq = require("mithril-query");
+var o = require("ospec");
 
-var SignUpComponent = require("../signup.js")
-var User = require("../user.js")
+var SignUpComponent = require("../signup.js");
+var User = require("../user.js");
 
 o.spec("SignUpComponent", function() {
   o("form is rendered when signed out", function() {
     User.authState = User.AuthState.SIGNED_OUT;
-    User.load = function() {}
+    User.load = function() {};
 
-    var out = mq(SignUpComponent)
+    var out = mq(SignUpComponent);
 
     out.should.have("form");
     out.should.have("div#signup-error");
@@ -18,10 +18,10 @@ o.spec("SignUpComponent", function() {
     out.should.have("input[type=password][placeholder=Password]");
     out.should.have("input[type=password][placeholder='Repeat password']");
     out.should.have("button[type=submit]");
-  })
+  });
   o("loading div is rendered when in default state", function() {
-    var out = mq(SignUpComponent)
+    var out = mq(SignUpComponent);
 
     out.should.have("div");
-  })
-})
+  });
+});
