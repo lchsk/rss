373a5d55c5bcece991f66a08d17fb96b10bc9f82
lchsk <mjlechowski@gmail.com>
2020-04-21 09:00:56 +0100

    Add basic categories/channel menu

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,11 +1,13 @@
 import m from "mithril";
 
-const SignUpComponent = require('./signup');
-const LoginComponent = require('./login');
-const LogoutComponent = require('./logout');
-const User = require('./user');
-const AddNewChannelComponent = require('./add_channel');
-var {getErrorMessage, getSingleError} = require("./error");
+const SignUpComponent = require("./signup");
+const LoginComponent = require("./login");
+const LogoutComponent = require("./logout");
+const User = require("./user");
+const AddNewChannelComponent = require("./add_channel");
+var { getErrorMessage, getSingleError } = require("./error");
+
+User.loadChannels();
 
 const UserComponent = {
   oninit: node => {
@@ -14,78 +16,131 @@ const UserComponent = {
   view: node => {}
 };
 
-function getLink(classes, href, anchor) {
-  return m("a" + classes, {
-            href: href,
-            oncreate: m.route.link,
-          }, anchor)
+function defDict(type) {
+  var dict = {};
+  return {
+    get: function(key) {
+      if (!dict[key]) {
+        dict[key] = type.constructor();
+      }
+      return dict[key];
+    },
+    dict: dict
+  };
 }
 
+var UserChannels = {
+  // oninit: User.loadChannels(),
+  view: node => {
+    var channels = defDict([]);
+    var categories = defDict({});
+
+    for (let i = 0; i < User.channels.length; i++) {
+      const categoryId = User.channels[i]["category_id"];
+      const categoryTitle = User.channels[i]["category_title"];
+
+      if (categoryId) {
+        categories.get(categoryId)["categoryTitle"] = categoryTitle;
+      } else {
+        categories.get(categoryId)["categoryTitle"] = "Without category";
+      }
+
+      channels.get(categoryId).push(User.channels[i]);
+    }
+
+    var channelsHtml = [];
+
+    for (const [categoryId, c1] of Object.entries(channels.dict)) {
+      console.log(categoryId);
+      channelsHtml.push(
+        getLink(
+          ".list-group-item .list-group-item-action .active",
+          "/user",
+          categories.dict[categoryId]["categoryTitle"]
+        )
+      );
+
+      for (const channel of c1) {
+        console.log("\t" + channel["channel_url"]);
+        channelsHtml.push(
+          getLink(
+            ".list-group-item .list-group-item-action",
+            "/user",
+            channel["channel_url"]
+          )
+        );
+      }
+    }
+
+    return m("div", [m("div.list-group", channelsHtml)]);
+  }
+};
+
+function getLink(classes, href, anchor) {
+  return m(
+    "a" + classes,
+    {
+      href: href,
+      oncreate: m.route.link
+    },
+    anchor
+  );
+}
 
 var Layout = {
   view: function(node) {
-
-	return m(".container-fluid", [
-	  m('.row.top-bar', [
-		m(".col-sm-3", [
-          m("div", "hello",)
-        ]),
-		m(".col-sm-6", [
-          m("div", "",)
-        ]),
-		m(".col-sm-3 .text-right", [
-          m("div", "user",)
-        ]),
-	  ]),
-      m('.row', [
-        m('.col-sm .text-center', [
+    return m(".container-fluid", [
+      m(".row.top-bar", [
+        m(".col-sm-3", [m("div", "hello")]),
+        m(".col-sm-6", [m("div", "")]),
+        m(".col-sm-3 .text-right", [m("div", "user")])
+      ]),
+      m(".row", [
+        m(".col-sm .text-center", [
           getLink(".btn .btn-primary", "/channels/new", "Add new channel"),
 
-          m('hr'),
+          m("hr"),
 
-          m('div', [
-            m('div.list-group', [
-              getLink(".list-group-item .list-group-item-action", "/user", "Option 1"),
-              getLink(".list-group-item .list-group-item-action", "/user", "Option 2"),
-              getLink(".list-group-item .list-group-item-action", "/user", "Option 3"),
-            ])
-          ]),
-
+          m(UserChannels)
+          // m('div', [
+          //   m('div.list-group', [
+          //     getLink(".list-group-item .list-group-item-action", "/user", "Option 1"),
+          //     getLink(".list-group-item .list-group-item-action", "/user", "Option 2"),
+          //     getLink(".list-group-item .list-group-item-action", "/user", "Option 3"),
+          //   ])
+          // ]),
         ]),
-        m('.col-sm-9', node.children),
+        m(".col-sm-9", node.children)
       ])
-    ])
+    ]);
   }
 };
-
 
 var BoxLayout = {
   view: function(node) {
-
-	return m(".text-center", [
+    return m(".text-center", [
       m(".center", [
-        m('.top', "Logo"),
-        m('', node.children),
-        m('.bottom', "Forgot password? SignUP"),
-      ]),
-	])
+        m(".top", "Logo"),
+        m("", node.children),
+        m(".bottom", "Forgot password? SignUP")
+      ])
+    ]);
   }
 };
-
 
 m.route.prefix("#!");
 
 m.route(document.body, "/", {
   "/": {
     render: function() {
-      console.log("route /")
-      return m(Layout)
+      console.log("route /");
+      return m(Layout);
     }
   },
   "/channels/new": {
     render: function() {
-      console.log("new channel")
-      return m(Layout, m(AddNewChannelComponent))
+      console.log("new channel");
+      return m(Layout, m(AddNewChannelComponent));
     }
   },
   "/login": {
--- fe/src/user.js
+++ fe/src/user.js
@@ -9,12 +9,36 @@ const UserAuthState = {
 var User = {
   AuthState: UserAuthState,
   data: {},
+  channels: [],
   authState: UserAuthState.UNKNOWN,
 
+  loadChannels: () => {
+    console.log("Loading channels");
+    return m
+      .request({
+        method: "GET",
+        url: "http://localhost:8000/api/users/current/channels",
+        withCredentials: true
+      })
+      .then(result => {
+        User.channels = result["user_channels"];
+        console.log("channels loaded", result);
+        // m.redraw();
+        // User.data = { u: result };
+        // User.authState = User.AuthState.SIGNED_IN;
+      })
+      .catch(e => {
+        console.log(e);
+        // User.data = { error: e };
+        // TODO: Check for status
+        // User.authState = User.AuthState.SIGNED_OUT;
+        // m.route.set("/login");
+      });
+  },
   load: () => {
     if (User.authState === UserAuthState.SIGNED_OUT) {
       // if (m.route.get() !== "/login" && m.route.get() !== "/signup") {
-        // m.route.set("/login");
+      // m.route.set("/login");
       // }
       return;
     } else if (User.authState === UserAuthState.SIGNED_IN) {
@@ -34,7 +58,7 @@ var User = {
         User.data = { error: e };
         // TODO: Check for status
         User.authState = User.AuthState.SIGNED_OUT;
-		// m.route.set("/login");
+        // m.route.set("/login");
       });
   }
 };
