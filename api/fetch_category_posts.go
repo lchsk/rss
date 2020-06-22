dcdaec58e86ebfd0e6ff0f9a2c3ef213a3b4dbd4
Maciej Lechowski <mjlechowski@gmail.com>
2020-06-22 13:18:37 +0100

    Move queries/remove queries file

--- api/fetch_category_posts.go
+++ api/fetch_category_posts.go
@@ -7,7 +7,6 @@ import (
 	"strconv"
 
 	"github.com/gorilla/mux"
-	"github.com/lchsk/rss/libs/channel"
 	"github.com/lchsk/rss/libs/posts"
 )
 
@@ -32,7 +31,7 @@ func handlerFetchCategoryPosts(w http.ResponseWriter, req *http.Request) {
 		page = 1
 	}
 
-	rows, err := DBA.DB.Query(channel.SqlFetchChannelsWithinCategoryTree, categoryId)
+	rows, err := DBA.DB.Query(SqlFetchChannelsWithinCategoryTree, categoryId)
 
 	if err != nil {
 		w.WriteHeader(400)
@@ -66,3 +65,29 @@ func handlerFetchCategoryPosts(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(inboxPosts)
 }
+
+const SqlFetchChannelsWithinCategoryTree = `
+with recursive subcategories as (
+select
+	id,
+	title,
+	parent_id
+from
+	categories c
+where
+	id = $1
+union
+select
+	c.id,
+	c.title,
+	c.parent_id
+from
+	categories c
+inner join subcategories s on
+	s.id = c.parent_id ) select
+	c.id
+from
+	subcategories s
+join channels c on
+	c.category_id = s.id
+`
